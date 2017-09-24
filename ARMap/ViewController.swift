99221934f46a91d217a17e5bfbdd1f2122219a6f
Mhnd Mousa <mhndmsa@gmail.com>
2017-09-24 02:16:18 -0400

    non working location manager

--- ARMap/ViewController.swift
+++ ARMap/ViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 import SceneKit
 import ARKit
+import MapKit
 import CoreLocation
+import Foundation
 
 
-class Location: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
-
+class ViewController: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate{
+    
     @IBOutlet weak var long: UILabel!
     @IBOutlet weak var lat: UILabel!
     @IBOutlet weak var count: UILabel!
@@ -21,11 +23,12 @@ class Location: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
     var timer = Timer()
     var counter = 0
     var locationManager: CLLocationManager!
+    var updateInfoLabelTimer: Timer?
     
-    struct myCameraCoordinate {
-        var x = Float()
-        var y = Float()
-        var z = Float()
+    struct coordinate {
+        var x = Double()
+        var y = Double()
+        var z = Double()
     }
     
     func scheduledTimerWithTimeInterval(){
@@ -38,73 +41,82 @@ class Location: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
                 if CLLocationManager.isRangingAvailable(){
                     let currentLocation = locationManager.location
                     long.text = String(describing: currentLocation?.coordinate.longitude)
-                    lat.text = String(describing: currentLocation?.coordinate.longitude)
+                    lat.text = String(describing: currentLocation?.coordinate.latitude)
                     counter += 1
                     count.text = String(describing: counter)
                 }
             }
         }
     }
+    
+   // Uses Map
     @objc func updateCoordinates(){
         let currentLocation = locationManager.location
         long.text = String(describing: currentLocation?.coordinate.longitude)
         lat.text = String(describing: currentLocation?.coordinate.longitude)
         counter += 1
+        print(counter)
         count.text = String(describing: counter)
     }
     
-    func getCameraCoordinate(sceneview: ARSCNView) -> myCameraCoordinate{
+    // Uses AR
+    func getCameraCoordinate(sceneview: ARSCNView) -> coordinate{
         let cameraTransform = sceneView.session.currentFrame?.camera.transform
         let cameraCoordinates = MDLTransform(matrix: cameraTransform!)
         
-        var cc = myCameraCoordinate()
-        cc.x = cameraCoordinates.translation.x
-        cc.y = cameraCoordinates.translation.y
-        cc.z = cameraCoordinates.translation.z
+        var cc = coordinate()
+        cc.x = Double(cameraCoordinates.translation.x)
+        cc.y = Double(cameraCoordinates.translation.y)
+        cc.z = Double(cameraCoordinates.translation.z)
         
         return cc
     }
-    
     
     @objc func buttonAction(sender: UIButton!) {
         let alert = UIAlertController(title: "Alert", message: "location added", preferredStyle: UIAlertControllerStyle.alert)
         alert.addAction(UIAlertAction(title: "Done", style: UIAlertActionStyle.default, handler: nil))
         self.present(alert, animated: true, completion: nil)
-        
+
         let arrow = SCNNode(geometry: SCNPyramid(width: 0.5, height: 0.5, length: 0.5))
         let cc  = getCameraCoordinate(sceneview: sceneView)
         arrow.position = SCNVector3(cc.x,cc.y+0.7,cc.z-0.1)
         arrow.pivot = SCNMatrix4MakeRotation(3.14,1,0,0)
         arrow.geometry!.firstMaterial?.diffuse.contents  = UIColor(red: 255.0 / 255.0, green: 20.0 / 255.0, blue: 147.0 / 255.0, alpha: 0.8)
-        
+
         let base = SCNNode(geometry: SCNBox(width: 0.3, height: 0.4, length: 0.3, chamferRadius: 0))
         base.position = SCNVector3(cc.x,cc.y+1.05,cc.z-0.1)
         base.pivot = SCNMatrix4MakeRotation(3.14,1,0,0)
         base.geometry!.firstMaterial?.diffuse.contents  = UIColor(red: 255.0 / 255.0, green: 20.0 / 255.0, blue: 147.0 / 255.0, alpha: 0.8)
-        
+
         let base2 = SCNNode(geometry: SCNBox(width: 0.3, height: 0.1, length: 0.3, chamferRadius: 0))
         base2.position = SCNVector3(cc.x,cc.y+0.0775,cc.z-0.1)
         base2.pivot = SCNMatrix4MakeRotation(3.14,1,0,0)
         base2.geometry!.firstMaterial?.diffuse.contents  = UIColor(red: 255.0 / 255.0, green: 20.0 / 255.0, blue: 147.0 / 255.0, alpha: 0.8)
-        
+
         sceneView.scene.rootNode.addChildNode(arrow)
         sceneView.scene.rootNode.addChildNode(base)
         sceneView.scene.rootNode.addChildNode(base2)
+
+
         
 
         
     }
+
+
+
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        updateCoordinates()
         locationManager = CLLocationManager()
         locationManager.delegate = self
         
         locationManager.desiredAccuracy = kCLLocationAccuracyBest
         locationManager.requestWhenInUseAuthorization()
-        
+
+        locationManager.requestAlwaysAuthorization()
         locationManager.startUpdatingLocation()
         scheduledTimerWithTimeInterval()
 
@@ -112,7 +124,32 @@ class Location: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
         sceneView.delegate = self
         sceneView.showsStatistics = true
 
+        
+        
+//        let coordinate = CLLocationCoordinate2D(latitude: 51.504571, longitude: -0.019717)
+//        let location = CLLocation(latitude: -83.716203, longitude: 42.292005)
+//        let image = UIImage(named: "pin.png")!
+//        let annotationNode = LocationAnnotationNode(location: location, image: image)
+//        addLocationNodeWithConfirmedLocation(locationNode: annotationNode)
 
+//        let loc = locationOnEarth(lat: 42.292005, long: -83.716203)
+        //        var x = currentLocation?.coordinate.latitude
+        //         long.text = String(describing: x)
+        //         lat.text = String(describing: currentLocation?.coordinate.longitude)
+        let currentLocation = locationManager.location
+       
+        let z_coordinate = locationOnEarth(lat:42.292005,long: -83.716203, currentLatitude: (currentLocation?.coordinate.latitude)!, currentLongitude: (currentLocation?.coordinate.longitude)!).z
+        let x_coordinate = locationOnEarth(lat:42.292005,long: -83.716203, currentLatitude: (currentLocation?.coordinate.latitude)!, currentLongitude: (currentLocation?.coordinate.longitude)!).x
+        
+        var y = locationOnEarth(lat:42.292005,long: -83.716203, currentLatitude: (currentLocation?.coordinate.latitude)!, currentLongitude: (currentLocation?.coordinate.longitude)!).y
+        
+        
+        
+        let arrow = SCNNode(geometry: SCNPyramid(width: 0.5, height: 0.5, length: 0.5))
+        arrow.position = SCNVector3(x_coordinate,0,z_coordinate)
+        arrow.pivot = SCNMatrix4MakeRotation(3.14,1,0,0)
+        arrow.geometry!.firstMaterial?.diffuse.contents  = UIColor(red: 255.0 / 255.0, green: 20.0 / 255.0, blue: 147.0 / 255.0, alpha: 0.8)
+        sceneView.scene.rootNode.addChildNode(arrow)
         
         let button = UIButton(frame: CGRect(x: self.view.frame.width/2 - 50, y: self.view.frame.height - 100, width: 100, height: 50))
         button.backgroundColor = .green
@@ -121,12 +158,65 @@ class Location: UIViewController, ARSCNViewDelegate, CLLocationManagerDelegate {
         
         self.view.addSubview(button)
         
+    }
+
+    func locationOnEarth( lat:Double, long:Double, currentLatitude:Double, currentLongitude:Double) -> coordinate{
         
-  
-    
-    
+        let lat = 42.292005
+        let long = -83.716203
+        
+        // let xOfInput = 2 * 6371000.0 * cos(lat) * cos(long)
+        let yOfInput = 6371000.0 * cos(lat) * sin(long)
+        // let zOfInput = 2 * 6371000.0 * sin(lat)
+        
+        let currentLong = currentLongitude
+        let currentLat = currentLatitude
+        
+        // let xOfCurrent = 2 * 6371000.0 * cos(currentLat) * cos(currentLong)
+        let yOfCurrent = 6371000.0 * cos(currentLat) * sin(currentLong)
+        // let zOfCurrent = 2 * 6371000.0 * sin(currentLat)
+        
+        let xFromCurrentToInput = 2 * 6371000.0 * ((lat - currentLat)/360)
+        let zFromCurrentToInput = 2 * 6371000.0 * ((long - currentLong)/360)
+        let yFromCurrentToInput = yOfInput - yOfCurrent
+        
+        var loc = coordinate()
+        loc.z = zFromCurrentToInput
+        loc.y = yFromCurrentToInput
+        loc.x = xFromCurrentToInput
+        return loc
+        
     }
     
+//    func locationOnEarth( lat:Double, long:Double) -> coordinate{
+//
+//        let lat = lat
+//        let long = long
+//
+//        let xOfInput = 6371000.0 * cos(lat) * cos(long)
+//        let yOfInput = 6371000.0 * cos(lat) * sin(long)
+//        let zOfInput = 6371000.0 * sin(lat)
+//
+//        let currentLocation = locationManager.location
+//
+//        let currentLong = currentLocation?.coordinate.longitude
+//        let currentLat = currentLocation?.coordinate.latitude
+//
+//        let xOfCurrent = 6371000.0 * cos(currentLat!) * cos(currentLong!)
+//        let yOfCurrent = 6371000.0 * cos(currentLat!) * sin(currentLong!)
+//        let zOfCurrent = 6371000.0 * sin(currentLat!)
+//
+//        let xFromCurrentToInput = xOfInput - xOfCurrent
+//        let zFromCurrentToInput = zOfInput - zOfCurrent
+//        let yFromCurrentToInput = yOfInput - yOfCurrent
+//
+//        var loc = coordinate()
+//        loc.z = Float(zFromCurrentToInput)
+//        loc.y = Float(yFromCurrentToInput)
+//        loc.x = Float(xFromCurrentToInput)
+//        return loc
+//
+//    }
     
     
     
